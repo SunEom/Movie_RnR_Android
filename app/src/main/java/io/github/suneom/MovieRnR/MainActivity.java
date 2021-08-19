d182a12b7907ab6f3846583b70471bb7a2c0f899
SunEom <dkxl0701@gmail.com>
2021-08-19 19:38:50 +0900

    Initial commit

---
+++ app/src/main/java/io/github/suneom/MovieRnR/MainActivity.java
@@ -0,0 +1,19 @@
+package io.github.suneom.MovieRnR;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
