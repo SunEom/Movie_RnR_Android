b750bac39822dfc867b187f56339edda9e60e0f1
SunEom <dkxl0701@gmail.com>
2021-08-20 17:43:35 +0900

    Updated search process along with custom action bar

--- app/src/main/java/io/github/suneom/MovieRnR/MainActivity.java
+++ app/src/main/java/io/github/suneom/MovieRnR/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.hardware.input.InputManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -46,42 +48,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        View v = menu.findItem(R.id.search_bar).getActionView();
-        if(v!= null){
-            EditText searchKeyword = v.findViewById(R.id.search_keyword_input);
-
-            if(searchKeyword != null){
-                searchKeyword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                    @Override
-                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                        searchFragment = null;
-                        searchFragment = new SearchFragment();
-
-                        Bundle bundle = new Bundle();
-                        bundle.putString("keyword", searchKeyword.getText().toString());
-
-                        searchFragment.setArguments(bundle);
-
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, searchFragment).commit();
-
-                        return true;
-                    }
-                });
-            }
-        }
-
-        return true;
-    }
-
-
 
     public void settingBasicUI(){
 
+        TextView actionBar_title = actionBar.findViewById(R.id.action_bar_title);
+        actionBar_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+            }
+        });
 
+        EditText actionBar_searchBar = actionBar.findViewById(R.id.search_keyword_input);
+        actionBar_searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                searchFragment = null;
+                searchFragment = new SearchFragment();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("keyword", actionBar_searchBar.getText().toString());
+
+                searchFragment.setArguments(bundle);
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, searchFragment).commit();
+
+                ((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                return true;
+            }
+        });
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // Status Bar 설정
