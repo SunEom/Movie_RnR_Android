271bda6fbea4edd80d55dbde798cd519ac33ff5a
SunEom <dkxl0701@gmail.com>
2021-08-24 14:14:30 +0900

    Created Detail Activity UI

---
+++ app/src/main/java/io/github/suneom/MovieRnR/activity/DetailActivity.java
@@ -0,0 +1,16 @@
+package io.github.suneom.MovieRnR.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.suneom.MovieRnR.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
