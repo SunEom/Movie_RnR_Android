383a5384919f57b97cfae9bbd24e538cff9d2669
SunEom <dkxl0701@gmail.com>
2021-08-26 11:41:17 +0900

    Created join fragment

--- app/src/main/java/io/github/suneom/MovieRnR/fragment/LogInFragment.java
+++ app/src/main/java/io/github/suneom/MovieRnR/fragment/LogInFragment.java
@@ -41,9 +41,8 @@ public class LogInFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id_value = id.getText().toString();
-                String password_value = password.getText().toString();
-                sRequest.requestLoginGet(getContext());
+                JoinFragment joinFragment = new JoinFragment();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, joinFragment).commit();
             }
         });
 
