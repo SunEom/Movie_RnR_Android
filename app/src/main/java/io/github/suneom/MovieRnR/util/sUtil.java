5e4e3a73ea6b24a5bad06869bec8cec2aa57f437
SunEom <dkxl0701@gmail.com>
2021-08-26 12:23:19 +0900

    Created util method

--- app/src/main/java/io/github/suneom/MovieRnR/util/sUtil.java
+++ app/src/main/java/io/github/suneom/MovieRnR/util/sUtil.java
@@ -1,5 +1,10 @@
 package io.github.suneom.MovieRnR.util;
 
+import android.content.Context;
+import android.content.DialogInterface;
+
+import androidx.appcompat.app.AlertDialog;
+
 public class sUtil {
     public static int createRandomMovieCardImageIndex(){
         double dValue = Math.random();
@@ -9,5 +14,26 @@ public class sUtil {
 
     public static String ParseDateTilDate(String date){
         return date.substring(0,10);
+    }
+
+    public static void CreateNewSimpleAlertDialog(Context context, String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        if(!title.equals("")){
+            builder.setTitle(title);
+        }
+        if(!message.equals("")){
+            builder.setMessage(message);
+        }
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+        return;
     }
 }
